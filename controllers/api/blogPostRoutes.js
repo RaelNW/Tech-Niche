88131b7af2b05f3a883bb4eb93c9b99cfe883044
Rael Wanjala <raelnwanjala@gmail.com>
2023-10-07 20:52:41 -0700

    Added a post and delete route in blogpost routes

--- controllers/api/blogPostRoutes.js
+++ controllers/api/blogPostRoutes.js
@@ -1,4 +1,34 @@
 const router = require("express").Router();
 const { BlogPost } = require("../../models");
+const withAuth = require("../../utils");
 
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const newBlogData = await BlogPost.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+    res.status(200).json(newBlogData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post("/:id", withAuth, async (req, res) => {
+  try {
+    const BlogData = await BlogPost.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!BlogData) {
+      res.status(404).json({ message: "Blog post not found with that id" });
+    }
+    res.status(200).json(BlogData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 module.exports = router;
